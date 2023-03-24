120499ed474748b7c533aed1f98098a224aa03b5
杨超 <war7ng@qq.com>
2023-03-25 06:30:54 +0800

    123123

--- .umirc.ts
+++ .umirc.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   outputPath: "docs",
   // base: "/#",
   // publicPath: '/static/',
-   publicPath: '/hook/',
+   publicPath: '/-/',
 
   proxy: {
     '/update': {
